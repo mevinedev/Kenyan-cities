124cd0089f02b5b1ce17d9e6355a2097c85b980d
mevinedev <72660182+mevinedev@users.noreply.github.com>
2020-10-29 22:37:25 +0300

    Update pie.R

--- pie.R
+++ pie.R
@@ -3,7 +3,7 @@ slices <- c(10, 12, 4, 16, 8)
 lbls = c("US", "UK", "Australia", "Germany", "France")
 pct <- round(slices/sum(slices)*100)
 lbls = paste(lbls, pct) # add percents to labels
-lbls = paste(lbls,"%",sep="") # ad % to labels
+lbls = paste(lbls,"%",sep="") # add % to labels
 pie(slices,labels = lbls, col=rainbow(length(lbls)),
     main="Pie Chart of Different Countries",
     )
